532944e494a38f9568336730ee95811930b61493
henrique <henriquecordeiro054@gmail.com>
2024-09-12 16:18:38 -0300

    update

--- app/src/components/BannerName/BannerName.jsx
+++ app/src/components/BannerName/BannerName.jsx
@@ -1,17 +1,22 @@
 
 import './BannerName.css'
-
+import {  GithubLogo, LinkedinLogo, } from 'phosphor-react';
 export default function BannerName() {
 
     return(
         <section className="banner">
-            <div className="container">
+            <div className="container"> 
                 <div className='text-image'>
-                    <h1>Henrique</h1>
-                    <h1>Cordeiroツ</h1>
+                    <h1>Henrique Devsツ</h1>
+                    <h2>Ei sou desenvolvedor Fullstack, Bem vindo ao meu portifolio!!</h2>
+                    <ul>
+                        <li><a href="https://github.com/HenriqueCDS" target="_blank"><span><GithubLogo size={32} /></span></a></li>
+                        <li><a href="https://www.linkedin.com/in/henrique-cordeiro-940709201/" target="_blank"><span><LinkedinLogo size={32} /></span></a></li>
+                    </ul>
+
                 </div>
                 <div className="image-banner">
-                    <img src="https://github.com/HenriqueCDS.png" />               
+                  <img src="https://github.com/HenriqueCDS.png"/>    
                 </div>
             </div>
         </section>
