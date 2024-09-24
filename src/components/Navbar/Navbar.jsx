6a0e3e41fe2fb5e0746732200578c1565ea3a958
henrique <henriquecordeiro054@gmail.com>
2024-09-24 12:50:50 -0300

    test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import './Navbar.css'
 export default function Navbar(prop) {
     return(
       <nav>
-        <div className="nav-item "><a href="index.html">Home</a></div>
-        <div className="nav-item"><a href="./pages/about.html">About</a></div>
-        <div className="nav-item"><a href="./pages/work.html">Projects</a></div>
-        <div className="nav-item"><a href="./pages/awards.html">Awards</a></div>
-        <div className="nav-item"><a href="./pages/contact.html">Contact</a></div>
+        <div className="nav-item "><a href="index.html">HOME</a></div>
+        <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
+        <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
+        <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
+        <div className="nav-item"><a href="./pages/contact.html">CONTACT</a></div>
       </nav>
     )
     
