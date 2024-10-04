5f5fc90c1b207951b2f0569c24a5be6952da5ce1
henrique <henriquecordeiro054@gmail.com>
2024-10-04 16:27:51 -0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
+import { Link } from 'react-router-dom'
+import Menu from '../Menu'
 import './Navbar.css'
 export default function Navbar(prop) {
     return(
       <nav>
-        <div className="nav-item "><a href="#container-banner">HOME</a></div>
-        <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
-        <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
-        <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
-        <div className="nav-item"><a href="./pages/contact.html">CONTACT</a></div>
+        <div className="desktop" >
+          <div className="nav-item "><a href="#container-banner">HOME</a></div>
+          <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
+          <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
+          <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
+          <div className="nav-item"><a href="#footerContainer">CONTACT</a></div>
+        </div>
+        <nav className="mobile">
+                    <div className="nav-items">
+                        <Link to={"/"}>Portfolio</Link>
+                    <div className="menu">
+                        <Menu />
+                     
+                    </div>
+                </div>
+            </nav>
       </nav>
+      
+
     )
     
 }
