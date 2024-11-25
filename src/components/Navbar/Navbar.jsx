cd45aac19d636db13178a1af2e540e86e9fb383e
henrique <henriquecordeiro054@gmail.com>
2024-11-25 16:33:24 -0300

    update versionamento

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import './Navbar.css'
 export default function Navbar(prop) {
     return(
       <nav>
-        <div className="nav-item "><a href="#container-banner">HOME</a></div>
-        <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
-        <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
-        <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
-        <div className="nav-item"><a href="./pages/contact.html">CONTACT</a></div>
+        <div className='desktop'>
+          <div className="nav-item "><a href="#container-banner">HOME</a></div>
+          <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
+          <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
+          <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
+          <div className="nav-item"><a href="./pages/contact.html">CONTACT</a></div>
+        </div>
+       
       </nav>
+      
     )
     
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,16 +13,18 @@ export default function Home() {
     return(
         <section>
             <Navbar page = {'home'}/>
-            
+
             <BannerName />
             
+            <TexBanner  text={"Showcasing My  Developer #Projects Portfolio Showcasing My Developer Portfolio Showcasing My  Developer #Projects  Portfolio"}/>
+            
+            <GridProjects />
+
             <TexBanner  text={"Showcasing My  Developer #Skills  Portfolio Showcasing My  Developer Portfolio  Showcasing My Developer  #Skills Portfolio"}/>
             
             <GridSkills />    
 
-            <TexBanner  text={"Showcasing My  Developer #Projects Portfolio Showcasing My Developer Portfolio Showcasing My  Developer #Projects  Portfolio"}/>
             
-            <GridProjects />
              
             <TexBanner  text={"Showcasing My #Contact Portfolio Showcasing My #Contact Portfolio Showcasing My #Contact Developer Portfolio"}/>
 
