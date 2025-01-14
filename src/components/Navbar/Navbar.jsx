5600232111892ee43580b047f3fb833b6355a6aa
henrique <henriquecordeiro054@gmail.com>
2025-01-14 09:52:42 -0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,10 @@ export default function Navbar(prop) {
     return(
       <nav>
         <div className='desktop'>
-          <div className="nav-item "><a href="#container-banner">HOME</a></div>
-          <div className="nav-item"><a href="./pages/about.html">ABOUT</a></div>
-          <div className="nav-item"><a href="./pages/work.html">PROJECTS</a></div>
-          <div className="nav-item"><a href="./pages/awards.html">SKILLS</a></div>
-          <div className="nav-item"><a href="./pages/contact.html">CONTACT</a></div>
+          <div className="nav-item "><a href="#banner">HOME</a></div>
+          <div className="nav-item"><a href="#gridProjects">PROJECTS</a></div>
+          <div className="nav-item"><a href="#container-skils">SKILLS</a></div>
+          <div className="nav-item"><a href="#footerContainer">CONTACT</a></div>
         </div>
        
       </nav>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,6 @@ export default function Home() {
 
     return(
         <section>
-            <Navbar page = {'home'}/>
 
             <BannerName />
             
