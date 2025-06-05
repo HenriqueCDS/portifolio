f62cbbab21d8d47c342072aa283b1f23a5a0e845
henrique <henriquecordeiro054@gmail.com>
2025-06-05 10:25:26 -0300

    update cell responsiv

--- src/components/GridSkills/Skills-Table/index.jsx
+++ src/components/GridSkills/Skills-Table/index.jsx
@@ -6,7 +6,7 @@ export default function SkillsTable(){
             <Skill title = "Linguagens" texts ={["TypeScript","Javascript","Java","Pyhton", ]} />
             <Skill title = "Databases" texts ={["MySql","Microsoft SQL Server","Mongodb" ]}  />
             <Skill title = "FrameWorks" texts ={["React","Express js","Next js","SpringBoot" ]} />
-            <Skill title = "Ferramentas" texts ={["Linux","Figma","Git","vscode","Eclipse" ]} />
+            <Skill title = "Ferramentas" texts ={["Linux","Docker","Figma","Git","vscode","Eclipse" ]} />
             <Skill title = "Outros" texts ={["Scrum","Java","Pyhton" ]} />
 
         </section>
