a706c9a35438c2d8b6716aab1ba49ed70dfd674c
henrique <henriquecordeiro054@gmail.com>
2025-07-04 10:49:19 -0300

    update loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
 
 import './App.css'
-
 import { Routes, BrowserRouter, Route } from  "react-router-dom";
 import Home from './pages/Home';
-import { useEffect,useState } from 'react';
-import Loader from './components/Loader';
+
 
 
 function App() {
-  const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false); 
-    }, 7500); 
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
-              {/* <Route path="/works" element={<Favoritos />} />
-              <Route path="/skils" element={<About titulo='Sobre Nos' />} />
-              <Route path="/contact" element={<Registro/>} />
-          <Route path="*" element={<NotFound />} /> */}
         </Routes>   
       </BrowserRouter>
     </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,28 +5,60 @@ import GridSkills from "../../components/GridSkills";
 import Navbar from "../../components/Navbar/Navbar";
 import GridProjects from "../../components/ProjectsGrid";
 import TexBanner from "../../components/TextBanner";
+import Loader from '../../components/Loader';
+import { useEffect,useState } from 'react';
 
 
 
 export default function Home() {
 
+    const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
+    
+      useEffect(() => {
+        const timer = setTimeout(() => {
+          setLoading(false); 
+        }, 8500); 
+    
+        return () => clearTimeout(timer);
+      }, []);
+
+
+      useEffect(() => {
+        if (loading) {
+            document.body.style.overflow = 'hidden'; // Impede scroll
+            
+        } else {
+            document.body.style.overflow = 'visible'; // Restaura scroll
+        }
+
+        return () => {
+            document.body.style.overflow = 'visible'; // Precaução para desmontar
+        };
+      }, [loading]);
+            
+            
+
+
     return(
         <section>
-            <Navbar />
-            <BannerName />
+        {loading && <Loader />}
             
-            <TexBanner  text={"Showcasing My  Developer #Projects Portfolio Showcasing My Developer Portfolio Showcasing My  Developer #Projects  Portfolio"}/>
-            
-            <GridProjects />
+        <div className={`page-content ${loading ? 'hidden' : 'visible'}`}>
+                <Navbar />
+                <BannerName />
+                
+                <TexBanner  text={"Showcasing My  Developer #Projects Portfolio Showcasing My Developer Portfolio Showcasing My  Developer #Projects  Portfolio"}/>
+                
+                <GridProjects />
 
-            <TexBanner  text={"Showcasing My  Developer #Skills  Portfolio Showcasing My  Developer Portfolio  Showcasing My Developer  #Skills Portfolio"}/>
+                <TexBanner  text={"Showcasing My  Developer #Skills  Portfolio Showcasing My  Developer Portfolio  Showcasing My Developer  #Skills Portfolio"}/>
+                
+                <GridSkills />    
             
-            <GridSkills />    
-        
-            <TexBanner  text={"Showcasing My #Contact Portfolio Showcasing My #Contact Portfolio Showcasing My #Contact Developer Portfolio"}/>
+                <TexBanner  text={"Showcasing My #Contact Portfolio Showcasing My #Contact Portfolio Showcasing My #Contact Developer Portfolio"}/>
 
-            <FootContacts />
-            
+                <FootContacts />
+            </div>
         </section>
     )
     
